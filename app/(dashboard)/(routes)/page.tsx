337e70a4b57f4dc25574c39acc919f28558da7fe
Shamar Morrison <shamar.webdev@gmail.com>
2023-10-08 02:03:36 -0700

    Added clerk

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <p className='text-3xl text-sky-700 font-medium'>Protected Home</p>
+}
--- app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <p className='text-3xl text-sky-700 font-medium'>Hello</p>
-}
